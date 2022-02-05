7fb7ede56a8dae0d0399b6375e1da99fc92dff61
Md. Tajminur Rahman <tajminur.aiub@gmail.com>
2022-02-05 13:15:31 +0600

    home ui update

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,10 +3,13 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div id="top-banner" className="flex">
-            <div className="max-w-6xl m-auto px-8 py-10 xl:py-20 bg-black opacity-60 xl:rounded-2xl">
-                <h1 className="pb-6 text-2xl md:tex-3xl xl:text-5xl font-bold text-gray-100 break-words uppercase">make your next vacation with wanderlust</h1>
-                <h4 className="xl:text-2xl font-semibold text-gray-300 break-words">You choose the destination, we handle the rest.</h4>
+        <div id="top-banner" className="flex flex-col justify-end">
+            <div className="w-full xl:max-w-5xl my-auto md:my-0 mx-auto px-8 xl:px-24 py-10 xl:py-20 bg-white bg-clip-padding bg-opacity-50  backdrop-filter backdrop-blur-xl shadow-lg xl:rounded-2xl">
+                <h1 style={{ lineHeight: "125%" }} className="pb-3 xl:pb-6 text-2xl xl:text-5xl font-semibold text-gray-700 leading-10">Plan Your Next Vacation With <br /><span className='text-green-500 font-bold uppercase'>wanderlust</span></h1>
+                <h4 className="xl:text-2xl font-semibold text-gray-500 break-words">You choose the destination, we handle the rest.</h4>
+            </div>
+            <div>
+                <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 400" xmlns="http://www.w3.org/2000/svg"><path d="M 0,400 C 0,400 0,200 0,200 C 89.866028708134,174.5071770334928 179.732057416268,149.01435406698562 269,162 C 358.267942583732,174.98564593301438 446.9377990430621,226.44976076555025 554,243 C 661.0622009569379,259.55023923444975 786.5167464114834,241.18660287081337 898,223 C 1009.4832535885166,204.81339712918663 1106.9952153110048,186.8038277511962 1195,183 C 1283.0047846889952,179.1961722488038 1361.5023923444976,189.5980861244019 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#fff"></path></svg>
             </div>
         </div>
     );
--- src/Pages/Home/Search/Search.js
+++ src/Pages/Home/Search/Search.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Search = () => {
     return (
-        <div className="flex">
-            <div className="container max-w-6xl mx-auto bg-green-500 py-12 xl:py-20 xl:-mt-32 xl:rounded-2xl">
+        <div className="flex bg-white">
+            <div className="container max-w-6xl mx-auto shadow-2xl bg-green-500 py-12 xl:py-20 xl:-mt-32 xl:rounded-2xl">
                 <div className="mx-auto">
-                    <h2 className="xl:text-3xl pt-4 pb-6 text-gray-200">Search Your Next Breathtaking Gateway</h2>
-                    <input className="w-2/3 xl:w-1/2 border-none rounded-lg py-2 xl:py-4 text-center" type="text" placeholder="Where do you want to go?" />
+                    <h2 className="xl:text-3xl pt-3 pb-3 text-gray-100">Search Your Next Breathtaking Gateway</h2>
+                    <input className="w-2/3 xl:w-1/2 rounded-lg py-2 xl:py-4 text-center appearance-none border border-transparent bg-gray-100 text-gray-700 placeholder-gray-600 shadow-xl text-base focus:outline-none focus:ring-2 focus:ring-gray-900 focus:border-transparent focus:bg-white" type="text" placeholder="Where do you want to go?" />
                 </div>
             </div>
         </div>
